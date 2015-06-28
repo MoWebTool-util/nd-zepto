f2c8cbec130cb3fae0d6f3bd95949fb4551325c4
lzhengms <lzhengms@gmail.com>
2015-06-28 16:50:08 +0800

    zepto

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @module: nd-zepto
+ * @author: lzhengms <lzhengms@gmail.com> - 2015-06-28 16:45:57
+ */
+
+'use strict';
+
+var Zepto;
+
+module.exports = require('./vendor/zepto');
---
+++ tests/index-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('expect.js');
+var Zepto = require('../index');
+
+/*globals describe, it*/
+
+describe('nd-zepto', function() {
+
+  it('normal usage', function() {
+
+  });
+
+});
